84e547f0797b9b67a57d8d550cd03bf274be3a9a
G <91590599+Geon-o@users.noreply.github.com>
2022-06-22 19:48:39 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package KH.study.practice;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        if (A > B) {
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else if (A == B) {
+            System.out.println("==");
+        }
+    }
+}
